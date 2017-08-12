581b402eb7714411b2c87a7707bf0328a437f325
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 14:32:23 +0100

    Added event validators in object creation

--- lib/event.rb
+++ lib/event.rb
@@ -4,10 +4,17 @@ attr_reader :id, :name, :price, :tickets_available
 attr_writer :id
 
     def initialize(params = {})
+        validate_arguments(params)
         id = nil
         @name = params.fetch(:name)
         @price = params.fetch(:price)
         @tickets_available = params.fetch(:tickets_available)
     end
 
+    private
+
+    def validate_arguments(params)
+        raise ArgumentError.new("Event requires price to be greater than 0") if params.fetch(:price) <= 0
+        raise ArgumentError.new("Event requires available tickets to be 0 or greater") if params.fetch(:tickets_available) < 0
+    end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -17,8 +17,22 @@ describe Event do
         expect(event.price).to eq 10
     end
 
+    it 'should throw error if ticket price <= 0' do
+        error_text = "Event requires price to be greater than 0"
+        expect{
+            described_class.new(:name => "Ed Sheeran - Live", :price => 0, :tickets_available => 1000)
+        }.to raise_error(ArgumentError, error_text)
+    end
+
     it 'should be able to return the events tickets available' do
       expect(event.tickets_available).to eq 1000
+    end
+
+    it 'should throw error if tickets available < 0' do
+        error_text = "Event requires available tickets to be 0 or greater"
+        expect{
+            described_class.new(:name => "Ed Sheeran - Live", :price => 10, :tickets_available => -1)
+        }.to raise_error(ArgumentError, error_text)
     end
   end
 
