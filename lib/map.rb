8377ef2a7f469766101d1b2536996cb2959cdfc3
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 15:05:47 +0100

    Map can store an event at a grid reference

--- lib/map.rb
+++ lib/map.rb
@@ -5,5 +5,10 @@ attr_reader :grid
     def initialize(n, val)
         @grid = Array.new(n) { Array.new(n) { val } }
     end
+    
+    def store_event(params = {})
+        grid_ref = params.fetch(:loc)
+        @grid[grid_ref[0]][grid_ref[1]] = params.fetch(:event)
+    end
 
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 require 'map'
 
 describe Map do
+    let(:event) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 1000) }
+
     subject (:map) { described_class.new(20, nil) }
 
     context 'size' do
         it 'should be able to return the maps size' do
             expect(map.grid.size).to eq 20
+        end
+    end
+    context 'storing' do
+        it  'should be able to store an event at a location' do
+            map.store_event(:event => event, :loc => ([4,2]))
+            expect(map.grid[4][2]).to eq event
         end
     end
 end
