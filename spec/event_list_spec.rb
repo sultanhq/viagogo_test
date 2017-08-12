5eb0eaed3099aa4aee3db9a0f6768f9b7e2861a9
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 13:56:08 +0100

    Event_List can store multiple events

--- spec/event_list_spec.rb
+++ spec/event_list_spec.rb
@@ -3,6 +3,8 @@ require 'event_list'
 
 describe Event_List do
     let(:event) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 1000) }
+    let(:event_2) { double("event", :id= => nil, :id => 2, :name => "Foo Fighters", :price => 20, :tickets_available => 2000) }
+    let(:event_3) { double("event", :id= => nil, :id => 3, :name => "Manu Chao", :price => 5, :tickets_available => 100) }
     subject (:event_list) { described_class.new() }
 
     context 'Event list' do
@@ -13,6 +15,14 @@ describe Event_List do
         it 'Event list should be able to store an event' do
             event_list.store_event(event)
             expect(event_list.number_of_events).to eq 1
+        end
+
+        it 'Event list should be able to store multiple events' do
+            event_list.store_event(event)
+            event_list.store_event(event_2)
+            event_list.store_event(event_3)
+            event_list.events.each{ |e| p e.id} 
+            expect(event_list.number_of_events).to eq 3
         end
 
         it 'Event should have id once stored' do
