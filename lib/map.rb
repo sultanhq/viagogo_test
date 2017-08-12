a76c41f09ea1531667be7f78852d5a0cea1eb754
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 19:49:11 +0100

    Changed grid from array to hash of objects

--- lib/map.rb
+++ lib/map.rb
@@ -1,22 +1,23 @@
 class Map
+  attr_reader :grid
 
-attr_reader :grid
-
-    def initialize(n, location)
-        @grid = Array.new(n) { Array.new(n) { location } }
+  def initialize(n, location)
+    @grid = {}
+    range = 0 - n..n
+    range.each do |ele|
+      range.each { |num| @grid[[num, ele]] = location }.each { |e| e }
     end
+  end
 
-    def store_event(params = {})
-        event = params.fetch(:event)
-        grid_ref = params.fetch(:loc)
-        @grid[grid_ref[0]][grid_ref[1]].add_event(event)
-    end
+  def store_event(params = {})
+    event = params.fetch(:event)
+    grid_ref = params.fetch(:loc)
+    @grid[grid_ref].add_event(event)
+  end
 
-    def events_at_location(params = {})
-        x = params.fetch(:x)
-        y = params.fetch(:y)
-
-        @grid[x[0]][y[1]].list_events
-    end
-
+  def events_at_location(params = {})
+    x = params.fetch(:x)
+    y = params.fetch(:y)
+    @grid[[x, y]].list_events
+  end
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -4,20 +4,22 @@ require 'map'
 describe Map do
     let(:event) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 1000) }
     let(:location) { double("location", :events => [] )}
+    grid_range = 10
 
-    subject (:map) { described_class.new(20, location) }
+    subject (:map) { described_class.new(grid_range, location) }
 
     context 'details' do
         it 'should be able to return the maps size' do
-            expect(map.grid.size).to eq 20
+            grid_size = (grid_range*2+1)*(grid_range*2+1)
+            expect(map.grid.size).to eq grid_size
         end
     end
     context 'storing' do
         it  'should be able to store an event at a location' do
             allow(location).to receive(:add_event).with(event)
-            allow(location).to receive(:list_events).and_return ([event])
+            allow(location).to receive(:list_events).and_return (event)
             map.store_event(:event => event, :loc => ([4,2]))
-            expect(map.events_at_location(:x => 4,:y => 2)).to eq [event]
+            expect(map.events_at_location(:x => 4,:y => 2)).to eq event
         end
     end
 end
