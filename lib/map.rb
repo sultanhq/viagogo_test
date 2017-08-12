76364453050df6d9d4d3970b187c2b845b66ab01
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 17:21:36 +0100

    Map now using location objects

--- lib/map.rb
+++ lib/map.rb
@@ -2,13 +2,21 @@ class Map
 
 attr_reader :grid
 
-    def initialize(n, val)
-        @grid = Array.new(n) { Array.new(n) { val } }
+    def initialize(n, location)
+        @grid = Array.new(n) { Array.new(n) { location } }
     end
-    
+
     def store_event(params = {})
+        event = params.fetch(:event)
         grid_ref = params.fetch(:loc)
-        @grid[grid_ref[0]][grid_ref[1]] = params.fetch(:event)
+        @grid[grid_ref[0]][grid_ref[1]].add_event(event)
+    end
+
+    def events_at_location(params = {})
+        x = params.fetch(:x)
+        y = params.fetch(:y)
+
+        @grid[x[0]][y[1]].list_events
     end
 
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -3,18 +3,21 @@ require 'map'
 
 describe Map do
     let(:event) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 1000) }
+    let(:location) { double("location", :events => [] )}
 
-    subject (:map) { described_class.new(20, nil) }
+    subject (:map) { described_class.new(20, location) }
 
-    context 'size' do
+    context 'details' do
         it 'should be able to return the maps size' do
             expect(map.grid.size).to eq 20
         end
     end
     context 'storing' do
         it  'should be able to store an event at a location' do
+            allow(location).to receive(:add_event).with(event)
+            allow(location).to receive(:list_events).and_return ([event])
             map.store_event(:event => event, :loc => ([4,2]))
-            expect(map.grid[4][2]).to eq event
+            expect(map.events_at_location(:x => 4,:y => 2)).to eq [event]
         end
     end
 end
