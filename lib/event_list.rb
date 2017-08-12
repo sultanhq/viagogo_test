34ffd7ef8bfd55d6f28b4bb881dcc77af2c6440d
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-12 13:42:12 +0100

    Event_List can now store an event

--- lib/event_list.rb
+++ lib/event_list.rb
@@ -6,4 +6,16 @@ class Event_List
     def number_of_events
         @events.size
     end
+
+    def store_event(event)
+        create_unique_event_id(event)
+        @events << event
+    end
+
+    private
+
+    def create_unique_event_id(event)
+        event.id = self.number_of_events + 1
+    end
+
 end
--- spec/event_list_spec.rb
+++ spec/event_list_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 require 'event_list'
 
 describe Event_List do
+    let(:event) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 1000) }
     subject (:event_list) { described_class.new() }
 
     context 'Event list' do
         it 'Event list should be empty on creation' do
             expect(event_list.number_of_events).to eq 0
         end
+
+        it 'Event list should be able to store an event' do
+            event_list.store_event(event)
+            expect(event_list.number_of_events).to eq 1
+        end
     end
+
+
 end
