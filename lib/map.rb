613aa7d5a92d9efd044bfa41a0efabbcf858921c
Rob Adams <robert.andrew.adams@gmail.com>
2017-08-13 18:59:54 +0100

    Able to show all events

--- lib/map.rb
+++ lib/map.rb
@@ -1,76 +1,84 @@
 require 'location'
 class Map
-  attr_reader :grid
+    attr_reader :grid
 
-  def initialize(world_range, location, event_list)
-    @locations = []
-    @event_list = event_list
-    @grid = {}
-    @world_range = world_range
-    range = (0 - world_range)..world_range
-    range.each do |ele|
-        range.each do |num|
-            grid[[num, ele]] = Location.new(:capacity => 1)
+    def initialize(world_range, location, event_list)
+        @locations = []
+        @event_list = event_list
+        @grid = {}
+        @world_range = world_range
+        range = (0 - world_range)..world_range
+        range.each do |ele|
+            range.each do |num|
+                grid[[num, ele]] = Location.new(:capacity => 1)
+            end
+        end
+        create_seed_locations(world_range)
+        store_events_at_random_locations
+    end
+
+    def number_of_events
+        @grid.count {|e| e[1].list_events.size > 0}
+    end
+
+    def show_all_events
+        locations = @grid.select{ |k,v|
+            # p v
+            v.list_events.size > 0
+        }
+        locations.collect {|key,value| value.list_events }.flatten.sort_by{|e| e.id}
+    end
+
+    def store_event(params = {})
+        event = params.fetch(:event)
+        grid_ref = params.fetch(:loc)
+        @grid[grid_ref].add_event(event)
+    end
+
+    def events_at_location(params = {})
+        x = params.fetch(:x)
+        y = params.fetch(:y)
+        @grid[[x, y]].list_events
+    end
+
+    def find_events_nearby(x,y)
+        user_location = [x,y]
+        close_locations_with_events = find_close_events(user_location)
+        print_distance_from_user(close_locations_with_events, user_location)
+    end
+
+    private
+
+    def print_distance_from_user(close_locations_with_events, user_location)
+        #itterate over close_locations_with_events and return the distance
+        #from user using manhattan distance calculation
+
+    end
+
+    def find_close_events(user_location)
+        #psudo code:
+
+        #find nearest events by spiralling out from the users location
+        #Insert sprial algorithum here
+
+        #Return list of close locations with events
+    end
+
+
+
+    def create_seed_locations(world_range)
+        prng = Random.new
+        @event_list.events.each do |_event|
+            rand_x = prng.rand(0 - world_range..world_range)
+            rand_y = prng.rand(0 - world_range..world_range)
+            @locations << [rand_x, rand_y]
         end
     end
-    create_seed_locations(world_range)
-    store_events_at_random_locations
-  end
 
-  def number_of_events
-    @grid.count {|e| e[1].list_events.size > 0}
-  end
-
-  def store_event(params = {})
-    event = params.fetch(:event)
-    grid_ref = params.fetch(:loc)
-    @grid[grid_ref].add_event(event)
-  end
-
-  def events_at_location(params = {})
-    x = params.fetch(:x)
-    y = params.fetch(:y)
-    @grid[[x, y]].list_events
-  end
-
-  def find_events_nearby(x,y)
-      user_location = [x,y]
-      close_locations_with_events = find_close_events(user_location)
-      print_distance_from_user(close_locations_with_events, user_location)
-  end
-
-  private
-
-  def print_distance_from_user(close_locations_with_events, user_location)
-      #itterate over close_locations_with_events and return the distance
-      #from user using manhattan distance calculation
-
-  end
-
-  def find_close_events(user_location)
-      #psudo code:
-
-      #find nearest events by spiralling out from the users location
-      #Insert sprial algorithum here
-
-      #Return list of close locations with events
-  end
-
-
-
-  def create_seed_locations(world_range)
-    prng = Random.new
-    @event_list.events.each do |_event|
-      rand_x = prng.rand(0 - world_range..world_range)
-      rand_y = prng.rand(0 - world_range..world_range)
-      @locations << [rand_x, rand_y]
-    end
-  end
-
-  def store_events_at_random_locations
+    def store_events_at_random_locations
         @locations.each_with_index { |e, i|
             store_event(:event => @event_list.events[i], :loc => e)
         }
-  end
+    end
 
 end
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 require 'map'
 
 describe Map do
-    let(:event_1) { double("event_1", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 100) }
-    let(:event_2) { double("event_2", :id= => nil, :id => 2, :name => "The Killers", :price => 100, :tickets_available => 100) }
-    let(:event_3) { double("event_3", :id= => nil, :id => 3, :name => "Floyd Mayweather", :price => 10, :tickets_available => 1300) }
-    let(:event_4) { double("event_4", :id= => nil, :id => 4, :name => "Reading Festival", :price => 100, :tickets_available => 1600) }
-    let(:event_4) { double("event_4", :id= => nil, :id => 4, :name => "Little Mix", :price => 40, :tickets_available => 700) }
-    let(:event_5) { double("event_5", :id= => nil, :id => 5, :name => "Darts Championship", :price => 80, :tickets_available => 700) }
-    let(:event_6) { double("event_6", :id= => nil, :id => 6, :name => "England vs Austrailia", :price => 70, :tickets_available => 100) }
-    let(:event_7) { double("event_7", :id= => nil, :id => 7, :name => "Elbow", :price => 50, :tickets_available => 500) }
-    let(:event_8) { double("event_8", :id= => nil, :id => 8, :name => "Harry Potter", :price => 5, :tickets_available => 10) }
-    let(:event_9) { double("event_9", :id= => nil, :id => 9, :name => "Ed Motta", :price => 10, :tickets_available => 200) }
-    let(:event_10) { double("event_10", :id= => nil, :id => 10, :name => "Micky Flanagan", :price => 12, :tickets_available => 1000) }
-    let(:event_list) { double("event_list", :events => [:event_1,:event_2,:event_3,:event_4,:event_5,:event_6,:event_7,:event_8,:event_9,:event_10]) }
+    let(:event_1) { double("event", :id= => nil, :id => 1, :name => "Ed Sheeran - Live", :price => 10, :tickets_available => 100) }
+    let(:event_2) { double("event", :id= => nil, :id => 2, :name => "The Killers", :price => 100, :tickets_available => 100) }
+    let(:event_3) { double("event", :id= => nil, :id => 3, :name => "Floyd Mayweather", :price => 10, :tickets_available => 1300) }
+    let(:event_4) { double("event", :id= => nil, :id => 4, :name => "Reading Festival", :price => 100, :tickets_available => 1600) }
+    let(:event_4) { double("event", :id= => nil, :id => 4, :name => "Little Mix", :price => 40, :tickets_available => 700) }
+    let(:event_5) { double("event", :id= => nil, :id => 5, :name => "Darts Championship", :price => 80, :tickets_available => 700) }
+    let(:event_6) { double("event", :id= => nil, :id => 6, :name => "England vs Austrailia", :price => 70, :tickets_available => 100) }
+    let(:event_7) { double("event", :id= => nil, :id => 7, :name => "Elbow", :price => 50, :tickets_available => 500) }
+    let(:event_8) { double("event", :id= => nil, :id => 8, :name => "Harry Potter", :price => 5, :tickets_available => 10) }
+    let(:event_9) { double("event", :id= => nil, :id => 9, :name => "Ed Motta", :price => 10, :tickets_available => 200) }
+    let(:event_10) { double("event", :id= => nil, :id => 10, :name => "Micky Flanagan", :price => 12, :tickets_available => 1000) }
+    let(:event_list) { double("event_list", :events => [event_1,event_2,event_3,event_4,event_5,event_6,event_7,event_8,event_9,event_10]) }
     let(:location) { double("location", :events => [] )}
     grid_range = 10
 
@@ -25,7 +25,7 @@ describe Map do
             allow(location).to receive(:list_events).and_return (1)
             allow(event_list).to receive(:number_of_events).and_return (10)
             grid_size = (grid_range*2+1)*(grid_range*2+1)
-            expect(map.grid.size).to eq grid_size
+            expect(subject.grid.size).to eq grid_size
         end
     end
     context 'storing' do
@@ -33,7 +33,12 @@ describe Map do
             allow(location).to receive(:add_event).exactly(10).times
             allow(location).to receive(:list_events).and_return (1)
             allow(event_list).to receive(:number_of_events).and_return (10)
-            expect(map.number_of_events).to eq 10
+            expect(subject.number_of_events).to eq 10
+        end
+    end
+    context 'displaying' do
+        it 'should be able to show all events' do
+            expect(subject.show_all_events).to eq event_list.events
         end
     end
 end
